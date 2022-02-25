d22caa6706e3001839d858d8cf630f91003e8b17
Camille0512 <lizx0512@connect.hku.hk>
2022-02-25 15:24:05 +0800

    NLA first commit

---
+++ Decomposition.py
@@ -0,0 +1,293 @@
+from copy import deepcopy
+from numpy.linalg import det
+from numpy import array, dot, diag, zeros, log, exp
+
+
+def trans_discount_factor_zero_rate(discount_factor: list, periods: list, period_type: str):
+    len_df, len_p = len(discount_factor), len(periods)
+    if len_df != len_p: raise ValueError("Wrong input, please check the length of the input lists.")
+
+    zero_rate = []
+    period_mapping = {
+        "day": 365,
+        "std_day": 360,
+        "week": 52,
+        "month": 12,
+        "semi": 2
+    }
+    for i in range(len_df):
+        # Continuous compounding
+        zr = log(1 / discount_factor[i]) * (period_mapping[period_type] / periods[i])
+        zero_rate.append(zr)
+    return zero_rate
+
+
+def compute_discount_factor(t: float, poly_params: dict):
+    """
+    Compute the discount factors.
+    :param t: The given time. For bonds, it should be the cash flow date.
+    :param poly_params: The polynomial parameters.
+    :return: The discount factor of given time.
+    """
+    a, b, c, d = 0, 0, 0, 0
+    for (l, r), params in poly_params.items():
+        if l <= t <= r: a, b, c, d = params
+    r = a + b * t + c * t * t + d * t * t * t
+    return exp(-t * r)
+
+
+class LU:
+    def __init__(self, A=None, L=None, U=None, P=None, b=None, interval=None, interval_value=None):
+        self.A = A
+        self.L = L
+        self.U = U
+        self.P = P
+        self.b = b
+        self.n = len(self.A) if self.A is not None else None
+        self.discount_factor_comp = {
+            "TM": self._dfc_tm,
+            "RP": self._dfc_rp
+        }
+
+    def create_clean_matrix(self):
+        """
+        Clear the matrix.
+        :return: A clean matrix.
+        """
+        if self.n is None: print("Error! Please input matrix size.")
+        return [[0] * self.n for i in range(self.n)]
+
+    def forward_substitution(self, L=None, b=None, d=6):
+        """
+        Solve the Lx=b equation by using forward substitution, given L and b.
+        :param L: The lower triangular matrix of the linear problem, in list format.
+        :param b: The vector for the result.
+        :param d: The decimal that would like to keep, default is 6 decimals.
+        :return: The solved x list.
+        """
+        if L is None: L = deepcopy(self.L)
+        if b is None: b = deepcopy(self.b)
+        n = len(L)
+        x = [None] * n
+        x[0] = b[0] / L[0][0]
+        for i in range(1, n):
+            numerator = b[i] - sum([j * x[e] for e, j in enumerate(L[i][:i])])
+            x[i] = round(numerator / L[i][i], d)
+        return x
+
+    def backward_substitution(self, U=None, b=None, d=6):
+        """
+        Solve the Ux=b equation by using forward substitution, given U and b.
+        :param U: The upper triangular matrix of the linear problem, in list format.
+        :param b: The vector for the result.
+        :param d: The decimal that would like to keep, default is 6 decimals.
+        :return: The solved x list.
+        """
+        if U is None: U = deepcopy(self.U)
+        if b is None: b = deepcopy(self.b)
+        n = len(U) - 1
+        x = [None] * (n + 1)
+        x[n] = b[n] / U[n][n]
+        for i in range(1, n + 1):
+            numerator = b[n - i] - sum([j * x[n - e] for e, j in enumerate(U[n - i][(n - i + 1):][::-1])])
+            x[n - i] = round(numerator / U[n - i][n - i], d)
+        return x
+
+    def _update_LU_A(self, A, ind: int):
+        """
+        The core computation of the LU decomposition.
+        :param A: Matrix A used in computatioon.
+        :param ind: The current updating index.
+        :return: No return. Update matrix A, L, U.
+        """
+        for k in range(ind, self.n):
+            self.U[ind][k] = A[ind][k]
+            self.L[k][ind] = A[k][ind] / A[ind][ind]
+        for j in range(ind + 1, self.n):
+            for k in range(ind + 1, self.n):
+                A[j][k] = A[j][k] - self.L[j][ind] * self.U[ind][k]
+        return A
+
+    def lu_no_pivoting(self, A=None):
+        """
+        LU decomposition without pivoting given non-singular matrix A.
+        Only exists if all the leading principal minors of the matrix are nonzero.
+        :param A: The given non-singular matrix A.
+        :return: The LU decomposition, lower triangular matrix L and upper triangular matrix U.
+        """
+        if A is None:
+            if self.A is None: raise ValueError("Please initiate A or input A for the method.")
+            A = deepcopy(self.A)
+        n = len(A)
+        verify = 1
+        for i in range(1, n + 1):
+            arr = array([a[:i] for a in A[:i]]).reshape(i, i)
+            verify *= det(arr)
+        if verify == 0:
+            print("The given matrix has no LU decomposition without pivoting.")
+            return
+
+        self.L, self.U = self.create_clean_matrix(), self.create_clean_matrix()
+        for i in range(0, n - 1):
+            A = self._update_LU_A(A, i)
+        self.L[n - 1][n - 1], self.U[n - 1][n - 1] = 1, A[n - 1][n - 1]
+        return self.L, self.U
+
+    def lu_row_pivoting(self, A=None, givenLU=False):
+        """
+        Compute the LU decomposition with row pivoting.
+        Any non-singular matrix has an LU decomposition with row pivoting.
+        :param A: The given non-singular matrix A.
+        :param givenLU: Whether use the previous given L & U matrix, default False (not use).
+        :return: The LU decomposition (lower triangular matrix L and upper triangular matrix U), permutation matrix P.
+        """
+        if A is None:
+            if self.A is None: raise ValueError("Please initiate A or input A for the method.")
+            A = deepcopy(self.A)
+        n = len(A)
+        if not givenLU: self.L, self.U = self.create_clean_matrix(), self.create_clean_matrix()
+        P = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
+
+        for i in range(0, n - 1):
+            # Find the largest absolute value and put it in the first one.
+            col = [elm[i] for elm in A[i:]]
+            ind = col.index(max(col)) + i
+            A[i], A[ind] = A[ind].copy(), A[i].copy()
+            P[ind], P[i] = P[i], P[ind]
+            if i > 0:
+                self.L[ind], self.L[i] = self.L[i].copy(), self.L[ind].copy()
+
+            A = self._update_LU_A(A, i)
+        self.P = P
+        self.L[n - 1][n - 1], self.U[n - 1][n - 1] = 1, A[n - 1][n - 1]
+        return self.L, self.U, self.P
+
+    def _dfc_tm(self, cate: str, M=None, b=None):
+        if b is None: b = deepcopy(self.b)
+        if cate.lower() == "fs":
+            return self.forward_substitution(M, b) if M is not None else self.forward_substitution(b)
+        elif cate.lower() == "bs":
+            return self.backward_substitution(M, b) if M is not None else self.forward_substitution(b)
+
+    def _dfc_rp(self, P=None, b=None):
+        if self.b is None: raise EOFError("Please give the vector b.")
+        if b is None: b = deepcopy(self.b)
+        if P is None:
+            if self.P is None: self.lu_row_pivoting()
+            P = deepcopy(self.P)
+        pb = list(dot(array(P), array(b)))
+        y = self.forward_substitution(b=pb)
+        return self.backward_substitution(b=y)
+
+    def compute_discount_factor(self, cate: str, M=None, b=None):
+        cate_list = cate.split("_")
+        func = self.discount_factor_comp[cate_list[0]]
+        return func(cate_list[1], M, b) if len(cate_list) > 1 is not None else func(M, b)
+
+    def compute_linear_system(self, B: list, A=None, givenLU=False):
+        """
+        Solve the linear system for the same given matrix.
+        :param B: The given results for the linear system (the right side of the equations).
+        :param A: The given matrix for the linear system.
+        :param givenLU: Whether use the previous given L & U matrix, default False (not use).
+        :return: The solution for the linear system.
+        """
+        if A is None:
+            if self.A is None: raise ValueError("Please initiate A or input A for the method.")
+            A = deepcopy(self.A)
+
+        if not givenLU: self.L, self.U = self.create_clean_matrix(), self.create_clean_matrix()
+        _, _, P = self.lu_row_pivoting(A=A)
+
+        B = array(B)
+        s = B.shape[1]
+        LS = diag(zeros(s))
+        for i in range(s):
+            v = dot(P, B[:, i])
+            y = self.forward_substitution(b=v)
+            x = self.backward_substitution(b=y)
+            LS[:, i] = x
+        res = [list(LS[row, :]) for row in range(s)]
+        return res
+
+    def _interpolation_matrix(self, intervals: list):
+        """
+        Generate the linear system for the cubic spline interpolation.
+        :param intervals: The intervals for the problem.
+        :return: The matrix to solve the cubic spline interpolation problem.
+        """
+        n = len(intervals) - 1
+        M = diag(zeros(n * n))
+        self.n = n * n
+
+        M[0][2], M[0][3] = 2, 6 * intervals[0]
+        for sec in range(n - 1):
+            # Equation 1
+            M[1 + sec * 4][0 + sec * 4] = 1
+            M[1 + sec * 4][1 + sec * 4] = intervals[sec]
+            M[1 + sec * 4][2 + sec * 4] = intervals[sec] * intervals[sec]
+            M[1 + sec * 4][3 + sec * 4] = intervals[sec] * intervals[sec] * intervals[sec]
+
+            # Equation 2
+            M[2 + sec * 4][0 + sec * 4] = 1
+            M[2 + sec * 4][1 + sec * 4] = intervals[sec + 1]
+            M[2 + sec * 4][2 + sec * 4] = intervals[sec + 1] * intervals[sec + 1]
+            M[2 + sec * 4][3 + sec * 4] = intervals[sec + 1] * intervals[sec + 1] * intervals[sec + 1]
+
+            # Equation 3
+            M[3 + sec * 4][1 + sec * 4] = 1
+            M[3 + sec * 4][2 + sec * 4] = 2 * intervals[sec + 1]
+            M[3 + sec * 4][3 + sec * 4] = 3 * intervals[sec + 1] * intervals[sec + 1]
+            M[3 + sec * 4][5 + sec * 4] = -1
+            M[3 + sec * 4][6 + sec * 4] = -2 * intervals[sec + 1]
+            M[3 + sec * 4][7 + sec * 4] = -3 * intervals[sec + 1] * intervals[sec + 1]
+
+            # Equation 4
+            M[4 + sec * 4][2 + sec * 4] = 2
+            M[4 + sec * 4][3 + sec * 4] = 6 * intervals[sec + 1]
+            M[4 + sec * 4][6 + sec * 4] = -2
+            M[4 + sec * 4][7 + sec * 4] = -6 * intervals[sec + 1]
+        M[-3][-4] = 1
+        M[-3][-3] = intervals[n - 1]
+        M[-3][-2] = intervals[n - 1] * intervals[n - 1]
+        M[-3][-1] = intervals[n - 1] * intervals[n - 1] * intervals[n - 1]
+        M[-2][-4] = 1
+        M[-2][-3] = intervals[n]
+        M[-2][-2] = intervals[n] * intervals[n]
+        M[-2][-1] = intervals[n] * intervals[n] * intervals[n]
+        M[-1][-2], M[-1][-1] = 2, 6 * intervals[n]
+        return M
+
+    def _interpolation_vect(self, f_x: list):
+        """
+        Generate the linear system for the cubic spline interpolation.
+        :param f_x: The value corresponding to each of the intervals.
+        :return: The matrix to solve the cubic spline interpolation problem.
+        """
+        if self.n is None: self.n = len(f_x) * len(f_x)
+        val = [0] * self.n
+        n = int(self.n ** 0.5)
+        val[0] = 0
+        for sec in range(n - 1):
+            val[sec * 4 + 1] = f_x[sec]
+            val[sec * 4 + 2] = f_x[sec + 1]
+            val[sec * 4 + 3], val[sec * 4 + 4] = 0, 0
+        val[-3], val[-2], val[-1] = f_x[-2], f_x[-1], 0
+        return val
+
+    def cubic_spline_interpolation(self, intervals: list, f_x: list):
+        """
+        To solve the cubic spline interpolation problem.
+        :param intervals: The intervals for the problem.
+        :param f_x: The value corresponding to each of the intervals.
+        :return: The parameters for the equations in each interval.
+        """
+        M = self._interpolation_matrix(intervals)
+        v = self._interpolation_vect(f_x)
+        self.b = v
+        _ = self.lu_row_pivoting(M)
+        x = self._dfc_rp()
+        equations = {}
+        for i in range(4):
+            equations[(intervals[i], (intervals[i + 1]))] = x[i * 4: (i + 1) * 4]
+        return x, equations
---
+++ run_script.py
@@ -0,0 +1,178 @@
+import numpy as np
+from Decomposition import LU, trans_discount_factor_zero_rate, compute_discount_factor
+
+np.set_printoptions(suppress=True)
+
+
+if __name__ == "__main__":
+    # # Question 1
+    # lu = LU()
+    #
+    # L = [
+    #     [100, 0, 0, 0],
+    #     [2, 102, 0, 0],
+    #     [3, 3, 103, 0],
+    #     [2, 2, 2, 102]
+    # ]
+    # b = [98.75, 102, 103.5, 105.5]
+    # x = lu.forward_substitution(L, b)
+    # print(x)
+    #
+    # L = [
+    #     [100, 0, 0, 0],
+    #     [6, 106, 0, 0],
+    #     [8, 8, 108, 0],
+    #     [5, 5, 5, 105]
+    # ]
+    # b = [98, 104, 111, 102]
+    # x = lu.forward_substitution(L, b)
+    # print("Forward decomposition")
+    # print(x)
+    # print("")
+    #
+    # U = [i[::-1] for i in L][::-1]
+    # b = b[::-1]
+    # x = lu.backward_substitution(U, b)
+    # print("Backward decomposition")
+    # print(x)
+    # print("")
+    #
+    # A = [
+    #     [2, -1, 3, 0],
+    #     [-4, 5, -7, -2],
+    #     [-2, 10, -4, -7],
+    #     [4, -14, 8, 10]
+    # ]
+    # lu.n = len(A)
+    # L, U = lu.lu_no_pivoting(A)
+    # print("LU decomposition")
+    # print(L)
+    # print(U)
+    # print("")
+    #
+    # A = [
+    #     [1, 101, 0, 0],
+    #     [2, 2, 102, 0],
+    #     [5, 0, 105, 0],
+    #     [2.5, 2.5, 2.5, 102.5]
+    # ]
+    # b = [100.6, 103.3, 107.3, 110.3]
+    # lu.n = len(A)
+    # L, U, P = lu.lu_row_pivoting(A)
+    # print("LU decomposition with row pivoting")
+    # print(L)
+    # print(U)
+    # print(P)
+    # print("")
+    #
+    # print("===========")
+    # print("Discount factors computation")
+    #
+    # x = lu.compute_discount_factor("RP", P, b)
+    # print(x)
+    # print("")
+    #
+    # lu.L = [
+    #     [100, 0, 0, 0],
+    #     [6, 106, 0, 0],
+    #     [8, 8, 108, 0],
+    #     [5, 5, 5, 105]
+    # ]
+    # lu.b = [98, 104, 111, 102]
+    # x = lu.compute_discount_factor("TM_fs")
+    # print(x)
+
+    # # Question 2
+    # A = [
+    #     [2, -1, 0, 1],
+    #     [-2, 0, 1, -1],
+    #     [4, -1, 0, 1],
+    #     [4, -3, 0, 2]
+    # ]
+    # L = [
+    #     [1, 0, 0, 0],
+    #     [1, 1, 0, 0],
+    #     [-0.5, 0.25, 1, 0],
+    #     [0.5, 0.25, 0, 1]
+    # ]
+    # U = [
+    #     [4, -1, 0, 1],
+    #     [0, -2, 0, 1],
+    #     [0, 0, 1, -0.75],
+    #     [0, 0, 0, 0.25]
+    # ]
+    # P = [
+    #     [0, 0, 1, 0],
+    #     [0, 0, 0, 1],
+    #     [0, 1, 0, 0],
+    #     [1, 0, 0, 0]
+    # ]
+    #
+    # b = [3, -1, 0, 2]
+    # lu = LU(A, L, U, P, b)
+    #
+    # x = lu.compute_discount_factor("RP")
+    # print(x)
+    #
+    # lu2 = LU(A, b=b)
+    # x = lu2.compute_discount_factor("RP")
+    # print(x)
+
+    # B = np.diag(np.ones(4))
+    # res = lu.compute_linear_system(list(B))
+    # print('res:\n', res)
+    #
+    # for i in range(4):
+    #     lu = LU(A, L, U, P, B[i])
+    #     x = lu.compute_discount_factor("RP")
+    #     print(x)
+
+    # # Question 3
+    # lu = LU()
+    # interval = [0, 2/12, 5/12, 11/12, 15/12]
+    # fx = [0.999973, 0.998, 0.9935, 0.982, 0.9775]
+    # x = lu.cubic_spline_interpolation(interval, fx)
+    # print("x:", x)
+    # for i in range(4):
+    #     print(x[i * 4 : (i + 1) * 4])
+
+    # # Question 4
+    disc_factor = [0.998, 0.9935, 0.982, 0.9775]
+    ps = [2, 5, 11, 15]
+    zr = [round(i, 6) for i in trans_discount_factor_zero_rate(disc_factor, ps, "month")]
+    # print(zr, "\n")
+
+    lu = LU()
+    interval = [0, 2 / 12, 5 / 12, 11 / 12, 15 / 12]
+    # print("intv:", interval)
+    fx = [0.01] + zr
+    print("fx:\n", fx)
+    x, eq = lu.cubic_spline_interpolation(interval, fx)
+    # print("v:\n", lu.b)
+
+    res, new_interval = 0, [1 / 12, 4 / 12, 7 / 12, 10 / 12, 13 / 12]
+    for i in range(5):
+        t = new_interval[i]
+        d = compute_discount_factor(t, eq)
+        print(d)
+        if i == 4: res += 100 * d
+        res += 0.625 * d
+    print(res)
+
+    # # # Question 5
+    # fx = [0.005, 0.0065, 0.0085, 0.0105, 0.012]
+    #
+    # lu = LU()
+    # interval = [0, 2 / 12, 6 / 12, 1, 20 / 12]
+    # # print("intv:", interval)
+    # x, eq = lu.cubic_spline_interpolation(interval, fx)
+    # print(eq)
+    #
+    # res, new_interval = 0, [1 / 12, 4 / 12, 7 / 12, 10 / 12, 13 / 12]
+    # for i in range(5):
+    #     t = new_interval[i]
+    #     d = compute_discount_factor(t, eq)
+    #     print(d)
+    #     if i == 4: res += 100 * d
+    #     res += 0.75 * d
+    # print(res)
